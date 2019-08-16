7c0348ee450b9d1c130efc8f1a436f8ba80b1a3e
mlch911 <mlch911@mluoc.tech>
2019-08-16 11:49:13 +0800

    Ver 0.2.2
    更改源

--- ssrpanel-script.sh
+++ ssrpanel-script.sh
@@ -4,12 +4,12 @@ export PATH
 #=================================================
 #	System Required: CentOS 7+
 #	Description: ssrpanel后端一键安装脚本
-#	Version: 0.2.1
+#	Version: 0.2.2
 #	Author: 壕琛
 #	Blog: http://mluoc.top/
 #=================================================
 
-sh_ver="0.2.1"
+sh_ver="0.2.2"
 github="https://raw.githubusercontent.com/mlch911/ssrpanel-script/master/ssrpanel-script.sh"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
@@ -128,7 +128,7 @@ Install_Shell(){
 		systemctl enable docker
 		systemctl start docker
 		cd /root
-		wget https://github.com/kszym2002/ssrpanel-be/releases/download/caddy-go/caddy-go.zip
+		wget https://github.com/mlch911/ssrpanel-be/releases/download/caddy-go/caddy-go.zip
 		unzip caddy-go.zip
 	fi
 
